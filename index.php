bd0ffbed65cbbe4006dee5b7574cccd25697b5d3
AlsconWeb <alsconweb_alex@outlook.com>
2023-03-27 18:52:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Index file.
+ */
+
+require_once __DIR__ . '/module_1.php';
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Module - 1</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Index file</h1>
+            <h2>call function getLocalTime()</h2>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
